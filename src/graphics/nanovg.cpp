1558528d95562107bbf00e46ad168d651e92afc2
ngoduyanh <ngoduyanh.chip@gmail.com>
2021-04-28 22:27:21 +0700

    Added some convenience methods to NvgContext

--- src/graphics/nanovg.cpp
+++ src/graphics/nanovg.cpp
@@ -11,5 +11,23 @@ ctn::NvgContext::NvgContext() {
 
 ctn::NvgContext::~NvgContext() { nvgDeleteGL3(m_ctx); }
 
-ctn::NvgContext::operator NVGcontext*() { return m_ctx; }
+void ctn::NvgContext::Rect(const Rect2 &rect) { Rect(rect.min, rect.max); }
+
+void ctn::NvgContext::Rect(Vec2 min, Vec2 max) {
+  nvgRect(m_ctx, min.x, min.y, max.x - min.x, max.y - min.y);
+}
+
+void ctn::NvgContext::Circle(const Circle2 &circle) {
+  Circle(circle.center, circle.radius);
+}
+
+void ctn::NvgContext::Circle(Vec2 center, float radius) {
+  nvgCircle(m_ctx, center.x, center.y, radius);
+}
+
+void ctn::NvgContext::MoveTo(Vec2 point) { nvgMoveTo(m_ctx, point.x, point.y); }
+
+void ctn::NvgContext::LineTo(Vec2 point) { nvgLineTo(m_ctx, point.x, point.y); }
+
+ctn::NvgContext::operator NVGcontext *() { return m_ctx; }
 
--- src/graphics/nanovg.hpp
+++ src/graphics/nanovg.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#include "nanovg.h"
-
+#include "../common/circle.hpp"
+#include "../common/rect.hpp"
 #include "glad/gl.h"
+#include "nanovg.h"
 
 #define NANOVG_GL3 1
 #include "nanovg_gl.h"
@@ -12,6 +13,16 @@ class NvgContext {
  public:
   NvgContext();
   ~NvgContext();
+
+  // Utility functions with Vec2, Rect2, etc.
+  void Rect(const Rect2& rect);
+  void Rect(Vec2 min, Vec2 max);
+
+  void Circle(const Circle2& circle);
+  void Circle(Vec2 center, float radius);
+
+  void MoveTo(Vec2 point);
+  void LineTo(Vec2 point);
 
   operator NVGcontext*();
 
